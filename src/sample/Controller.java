b90795dacf8523c9ae68437c53b022c0946842cb
Philip Schneider <philipschneider42@gmail.com>
2019-08-12 09:54:02 +0200

    Some Stuff is working...

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,72 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class Controller {
+    @FXML
+    JFXTextField numbers;
+    ScriptEngineManager scriptEngineManager;
+    ScriptEngine scriptEngine;
+
+    public void initialize() {
+        scriptEngineManager = new ScriptEngineManager();
+        scriptEngine = scriptEngineManager.getEngineByName("js");
+    }
+
+
+    public void onKeyReleased(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            calc();
+        }
+    }
+
+    public void calc() {
+        try {
+            String eval = numbers.getText();
+            eval = eval.replace("÷", "/");
+            eval = eval.replace("×", "*");
+
+            numbers.setText(scriptEngine.eval(eval).toString());
+        } catch (ScriptException e) {
+            numbers.setText("ERROR");
+            e.printStackTrace();
+        }
+    }
+
+    public void buttonClick(ActionEvent actionEvent) {
+        String input = ((JFXButton) actionEvent.getSource()).getText();
+        switch (input) {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "+":
+            case "-":
+            case "×":
+            case "÷":
+                numbers.setText(numbers.getText() + input);
+                break;
+            case "=":
+                calc();
+                break;
+            case "C":
+                numbers.setText("");
+                break;
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,28 +7,28 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Taschenrechner");
         JFXDecorator decorator = new JFXDecorator(primaryStage, root);
         decorator.setCustomMaximize(true);
         decorator.setGraphic(new SVGGlyph(""));
-        Scene scene = new Scene(decorator, 600, 900);
+        Scene scene = new Scene(decorator, 600, 900, true, SceneAntialiasing.BALANCED);
         final ObservableList<String> stylesheets = scene.getStylesheets();
         stylesheets.addAll(getClass().getResource("/css/jfoenix-fonts.css").toExternalForm(),
                 getClass().getResource("/css/jfoenix-design.css").toExternalForm(),
                 getClass().getResource("/css/custom.css").toExternalForm(),
-                getClass().getResource("/css/jfoenix-main-demo.css").toExternalForm());
+                getClass().getResource("/css/jfoenix-main-demo.css").toExternalForm(),
+                getClass().getResource("/css/taschenrechner.css").toExternalForm());
 
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
