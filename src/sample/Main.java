5d0c6032bd568e845144ce991de0eaca8da38cde
Philip Schneider <philipschneider42@gmail.com>
2019-08-12 07:51:47 +0200

    Added Style

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,9 @@
 package sample;
 
+import com.jfoenix.controls.JFXDecorator;
+import com.jfoenix.svg.SVGGlyph;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,8 +14,18 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Taschenrechner");
+        JFXDecorator decorator = new JFXDecorator(primaryStage, root);
+        decorator.setCustomMaximize(true);
+        decorator.setGraphic(new SVGGlyph(""));
+        Scene scene = new Scene(decorator, 600, 900);
+        final ObservableList<String> stylesheets = scene.getStylesheets();
+        stylesheets.addAll(getClass().getResource("/css/jfoenix-fonts.css").toExternalForm(),
+                getClass().getResource("/css/jfoenix-design.css").toExternalForm(),
+                getClass().getResource("/css/custom.css").toExternalForm(),
+                getClass().getResource("/css/jfoenix-main-demo.css").toExternalForm());
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
